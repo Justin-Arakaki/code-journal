87fe252bef12c3d8bd78df4a2a3e365439e8a7db
Justin-Arakaki <Justiniankoi@gmail.com>
2022-02-17 16:15:48 -0800

    Delete feature first draft

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 /* global data */
 /* exported data */
 const $form = document.querySelector('#entry-form');
-$form.addEventListener('submit', handleSubmit);
+$form.addEventListener('submit', handleClickSubmit);
 const $photoInput = document.querySelector('#photo');
 $photoInput.addEventListener('input', handlePhotoChange);
 const $imagePreview = document.querySelector('.img-preview');
 const $newButton = document.querySelector('.new');
-$newButton.addEventListener('click', handleNewClick);
+$newButton.addEventListener('click', handleClickNew);
 const $entryList = document.querySelector('[data-view="entries"] > ul');
-$entryList.addEventListener('click', editView);
+$entryList.addEventListener('click', handleClickEdit);
 const $emptyCase = document.querySelector('[data-view="entries"] > h4');
-document.addEventListener('DOMContentLoaded', populateEntries);
+document.addEventListener('DOMContentLoaded', handleLoad);
+const $overlay = document.querySelector('.overlay');
+const $deleteButton = document.querySelector('.delete');
+$deleteButton.addEventListener('click', handleClickDelete);
+const $cancelDeleteButton = document.querySelector('.cancel');
+$cancelDeleteButton.addEventListener('click', handleClickCancelDelete);
+const $confirmDeleteButton = document.querySelector('.confirm');
+$confirmDeleteButton.addEventListener('click', handleClickConfirmDelete);
 let editIndex = null;
 let $targetUpdated = null;
 
-function handleSubmit(e) {
+function handleLoad() { // Loads all information to html
+  for (let i = 0; i < data.entries.length; i++) {
+    $entryList.appendChild(renderEntry(data.entries[i]));
+  }
+  if (data.nextEntryId === 1) {
+    $emptyCase.className = 'font-weight-normal text-justify-center';
+  }
+  switchView(data.view);
+  autofillEdit(data.editing);
+}
+
+function handleClickSubmit(e) { // When submitting form on edit entry
   e.preventDefault();
   const input = e.target.elements;
   const newEntry = {
@@ -28,48 +46,93 @@ function handleSubmit(e) {
     data.entries.unshift(newEntry);
     data.nextEntryId++;
     $entryList.insertBefore(renderEntry(data.entries[0]), $topEntry);
-  } else {
-    if (editIndex === null || $targetUpdated === null) {
-      editIndex = searchEntries(data.editing);
-      $targetUpdated = $entryList.firstChild;
-      for (let i = 0; i < editIndex; i++) {
-        $targetUpdated = $targetUpdated.nextSibling;
-      }
-    }
-    data.entries[editIndex] = newEntry;
-    $entryList.replaceChild(renderEntry(newEntry), $targetUpdated);
-    data.editing = null;
-    editIndex = null;
   }
+  updateEditVars();
+  data.entries[editIndex] = newEntry;
+  $entryList.replaceChild(renderEntry(newEntry), $targetUpdated);
+  data.editing = null;
   e.target.reset();
+  clearEditVars();
+  clearInputs();
   $emptyCase.className = 'hidden';
   switchView('entries');
 }
 
-function searchEntries(entryObject) { // Returns index of entry in data.entries
-  for (const x in data.entries) {
-    if (data.entries[x] === entryObject) {
-      return x;
+function handleClickEdit(e) { // When clicking the pen icon
+  const clickee = e.target;
+  if (clickee.tagName === 'I') {
+    const id = clickee.closest('li').getAttribute('data-entry-id');
+    for (const x in data.entries) {
+      const entry = data.entries[x];
+      if (id === entry.entryId.toString()) {
+        data.editing = entry;
+        editIndex = x;
+        $targetUpdated = clickee.closest('li');
+        autofillEdit(entry);
+      }
     }
+    switchView('edit-entry');
   }
-  return -1;
 }
 
-function handlePhotoChange(e) { // Changes the image
+function handlePhotoChange(e) { // Changes the image preview
   const input = e.target.value;
   $imagePreview.src = input;
 }
 
-function handleNewClick() {
+function handleClickNew() { // When clicking new button
   switchView('entry-form');
   imgDefault();
+}
+
+function handleClickDelete() {
+  $overlay.className = 'overlay';
+}
+
+function handleClickCancelDelete() {
+  $overlay.className = 'overlay hidden';
+}
+
+function handleClickConfirmDelete() {
+  const index = searchEntries(data.editing);
+  data.entries.splice(index, 1);
+  data.editing = null;
+  $overlay.className = 'overlay hidden';
+  updateEditVars();
+  $targetUpdated.remove();
+  clearEditVars();
+  switchView('entries');
+}
+
+function updateEditVars() {
+  if (editIndex === null || $targetUpdated === null) {
+    editIndex = searchEntries(data.editing);
+    $targetUpdated = $entryList.firstChild;
+    for (let i = 0; i < editIndex; i++) {
+      $targetUpdated = $targetUpdated.nextSibling;
+    }
+  }
+}
+
+function clearEditVars() {
+  editIndex = null;
+  $targetUpdated = null;
+}
+
+function clearInputs() { // Fixes input values changed via JS
+  const $title = document.querySelector('#title');
+  const $photo = document.querySelector('#photo');
+  const $notes = document.querySelector('#notes');
+  $title.setAttribute('value', '');
+  $photo.setAttribute('value', '');
+  $notes.textContent = '';
 }
 
 function imgDefault() { // Catches errors with the image
   $imagePreview.src = 'images/placeholder-image-square.jpg';
 }
 
-function switchView(view) {
+function switchView(view) { // Changes html to the view (string)
   const $topLoc = document.querySelector('header h4');
   const $viewName = document.querySelector('header h1');
   const $view = document.querySelectorAll('[data-view]');
@@ -86,6 +149,7 @@ function switchView(view) {
       $topLoc.textContent = '';
       $viewName.textContent = 'New Entry';
       $newButton.className = 'new hidden';
+      $deleteButton.className = 'delete visibility-hidden';
       data.view = 'entry-form';
       break;
     case 'entries':
@@ -98,29 +162,13 @@ function switchView(view) {
       $topLoc.textContent = 'Entries';
       $viewName.textContent = 'Edit Entry';
       $newButton.className = 'new hidden';
+      $deleteButton.className = 'delete';
       data.view = 'edit-entry';
       break;
   }
 }
 
-function editView(e) {
-  const clickee = e.target;
-  if (clickee.tagName === 'I') {
-    const id = clickee.closest('li').getAttribute('data-entry-id');
-    for (const x in data.entries) {
-      const entry = data.entries[x];
-      if (id === entry.entryId.toString()) {
-        data.editing = entry;
-        editIndex = x;
-        $targetUpdated = clickee.closest('li');
-        populateEdit(entry);
-      }
-    }
-    switchView('edit-entry');
-  }
-}
-
-function populateEdit(entryObject) {
+function autofillEdit(entryObject) { // Populates inputs with selected entry
   const $titleInput = document.querySelector('#title');
   const $notesInput = document.querySelector('#notes');
   $titleInput.setAttribute('value', entryObject.title);
@@ -129,7 +177,16 @@ function populateEdit(entryObject) {
   $imagePreview.src = entryObject.photo;
 }
 
-function renderEntry(entry) {
+function searchEntries(entryObject) { // Returns index of entry in data.entries
+  for (const x in data.entries) {
+    if (data.entries[x] === entryObject) {
+      return x;
+    }
+  }
+  return -1;
+}
+
+function renderEntry(entry) { // Returns entry element
   const $row = document.createElement('li');
   $row.setAttribute('class', 'row entry-item');
   $row.setAttribute('data-entry-id', entry.entryId);
@@ -160,17 +217,6 @@ function renderEntry(entry) {
   $titleRow.appendChild($edit);
   $colInfo.appendChild($notes);
   return $row;
-}
-
-function populateEntries() {
-  for (let i = 0; i < data.entries.length; i++) {
-    $entryList.appendChild(renderEntry(data.entries[i]));
-  }
-  if (data.nextEntryId === 1) {
-    $emptyCase.className = 'font-weight-normal text-justify-center';
-  }
-  switchView(data.view);
-  populateEdit(data.editing);
 }
 
 imgDefault(); // to escape the lint
